272d5c16cfb7379e368b6107578c4664d8001334
lulueve3 <89736351+lulueve3@users.noreply.github.com>
2023-05-15 13:00:11 +0700

    THêm lại thể loại

--- app/src/main/java/com/example/foodapp/fragment/AddFragment.java
+++ app/src/main/java/com/example/foodapp/fragment/AddFragment.java
@@ -1,6 +1,7 @@
 package com.example.foodapp.fragment;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,8 +11,21 @@ import android.widget.Spinner;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.foodapp.API.APIService;
+import com.example.foodapp.API.RetrofitClient;
+import com.example.foodapp.Adapter.CategoryAdapter;
+import com.example.foodapp.Model.Category;
 import com.example.foodapp.R;
+import com.example.foodapp.SharedPrefManager;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class AddFragment extends Fragment {
     private View mView;
@@ -22,14 +36,38 @@ public class AddFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         mView = inflater.inflate(R.layout.fragment_add, container, false);
 
+        String accessToken = SharedPrefManager.getInstance(getContext()).getAccessToken();
+
         // Khởi tạo Spinner và ArrayAdapter
         mCategorySpinner = mView.findViewById(R.id.spCategory);
         mCategoryAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item);
 
-        // Thêm dữ liệu vào adapter
-        mCategoryAdapter.add("Thể loại 1");
-        mCategoryAdapter.add("Thể loại 2");
-        mCategoryAdapter.add("Thể loại 3");
+        APIService apiService = RetrofitClient.getInstant2();
+        String authorization = "Bearer " + accessToken;
+        apiService.getCategory(authorization).enqueue(new Callback<List<Category>>() {
+            @Override
+            public void onResponse(Call<List<Category>> call, Response<List<Category>> response) {
+                Log.d("API Response", "Categories: " + response);
+                if (response.isSuccessful()) {
+                    Log.d("API Response", "Categories: " + response);
+
+                    List<Category> categories = response.body();
+                    for (Category category: categories
+                         ) {
+                        mCategoryAdapter.add(category.getName());
+                    }
+
+                } else {
+                    Log.d("API Response", "Thất bại");
+                }
+
+            }
+            @Override
+            public void onFailure(Call<List<Category>> call, Throwable t) {
+
+            }
+        });
+
 
         // Đặt adapter cho Spinner
         mCategorySpinner.setAdapter(mCategoryAdapter);
